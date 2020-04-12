615df20de91958690362da998d8044713d6382e8
samuel-narvaez <Snarvaezperez47@gmail.com>
2020-04-12 00:15:46 -0500

    feat: Crud student

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,28 @@ function student() {
   return `SELECT * FROM student`;
 }
 
+function studentById(id) {
+  return `SELECT * FROM student WHERE id_student = ${id}`;
+}
+
+function add(body) {
+  return `INSERT INTO student (nombres, apellidos, cedula)
+  VALUES ('${body.nombres}','${body.apellidos}',${body.cedula} )`;
+}
+
+function update(body, id) {
+  return `UPDATE student SET
+  nombres = '${body.nombres}', 
+  apellidos = '${body.apellidos}', 
+  cedula = ${body.cedula}
+  WHERE id_student = ${id}`;
+}
+
+function destroy(id) {
+  return `DELETE FROM student WHERE id_student = ${id}`;
+}
+
+
 router.get('/student', async(req, res) => {
   const query = student();
   try {
@@ -22,6 +44,80 @@ router.get('/student', async(req, res) => {
     })
   }
 });
+
+
+router.get('/student/:id', async(req, res) => {
+  let id =  req.params.id;
+  const query = studentById(id);
+  try {
+    const Student = await Connection.query(query);
+    res.status(200).json({
+      message: 'Datos Obtenidos Correctamente',
+      Student: Student[0]
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ocurrio un error',
+      error : error.message
+    })
+  }
+});
+
+
+router.post('/student', async(req, res) => {
+  const body = req.body;  
+  const query = add(body);
+
+  try {
+    await Connection.query(query);
+    res.status(200).json({
+      message: 'Datos Guardados Correctamente',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ocurrio un error',
+      error
+    })
+  }
+});
+
+
+router.put('/student/:id', async(req, res) => {
+  let id =  req.params.id;
+  const body = req.body;
+  const query = update(body, id);
+  try {
+    await Connection.query(query);
+    res.status(200).json({
+      message: 'Datos Actualizados Correctamente',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ocurrio un error',
+      error : error.message
+    })
+  }
+});
+
+
+router.delete('/student/:id', async(req, res) => {
+  let id =  req.params.id;
+  const query = destroy(id);
+  try {
+    await Connection.query(query);
+    res.status(200).json({
+      message: 'Dato Eliminado Correctamente',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ocurrio un error',
+      error : error.message
+    })
+  }
+});
+
+
+
 
 
 
